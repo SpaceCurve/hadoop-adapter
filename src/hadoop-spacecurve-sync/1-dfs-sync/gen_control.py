e4ca840675dda044c561c1dd735513cdec7d7b9c
sjg5d8 <sjg5d8@users.noreply.github.com>
2014-10-10 15:10:52 -0700

    create missing dir, comments in upload script

--- src/hadoop-spacecurve-sync/1-dfs-sync/gen_control.py
+++ src/hadoop-spacecurve-sync/1-dfs-sync/gen_control.py
@@ -25,9 +25,15 @@ def gen_control(sql,uq,job):
 
 
 def main(job,resolution='4h0'):
+    if not os.path.isdir('control-dir'):
+        os.mkdir('control-dir')
+
+    # generate a list of queries for the cells in the Discrete Global Grid 
+    # http://discreteglobalgrids.org/
     queries = dgg.generate_quoted_queries(resolution)
 
     for i,control in enumerate(gen_control(sql,uq,job) for sql,uq in queries):
+
         f = open("control-dir/control-%04d.json" % (i,), "w")
         f.write(json.dumps(control))
         f.close()
--- src/hadoop-spacecurve-sync/3-hive-to-scdb/upload-direct.sh
+++ src/hadoop-spacecurve-sync/3-hive-to-scdb/upload-direct.sh
@@ -10,4 +10,5 @@ hdfs dfs -cat /apps/hive/warehouse/job2_earthquake_agg/00* \
     | ./geo_filter.py \
     | curl -f --data-binary @- $SPACECURVE_IP:8080/ArcGIS/schema/hiveresult
 
+# print out the number of rows in hiveresult, confirms the upload
 curl -s $SPACECURVE_IP:8080/ArcGIS/schema/hiveresult | wc -l
